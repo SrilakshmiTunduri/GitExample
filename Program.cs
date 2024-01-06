1557b9b27083bf73933e5d889593898ec59fb830
SrilakshmiTunduri <srilakshmitunduri@gmail.com>
2024-01-06 22:25:44 +0530

    console.writeline modified

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace GitExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello c# world");
+            
         }
     }
 }
